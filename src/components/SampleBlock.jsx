f247143c24368a9f6a37cb7355784040b0cced0f
Spyros <avspyros@gmail.com>
2023-07-18 13:08:40 +0300

    sample component layout ok

--- src/components/SampleBlock.jsx
+++ src/components/SampleBlock.jsx
@@ -1,12 +1,14 @@
 export default function SampleBlock({ title, description }) {
   return (
     <div className="sample-block">
-      <div className="preview-container">
-        <img src="https://fakeimg.pl/400x350" alt="sample-preview" />
+      <div className="preview-container" style={{ backgroundImage: `url("https://fakeimg.pl/600x600")` }}>
+        <a href="#"></a>
       </div>
       <div className="excerpt-container">
-        <h3>Sample Title</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi quo sed illum magnam repudiandae, eius suscipit quisquam dolores incidunt sapiente sequi ducimus. Magnam eos minima repellendus sapiente commodi deleniti.</p>
+        <div className="inner">
+          <h2>Sample Title</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio nisi quo sed illum magnam repudiandae, eius suscipit quisquam dolores incidunt sapiente sequi ducimus.</p>
+        </div>
       </div>
     </div>
   )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -9,7 +9,7 @@ export default function Welcome() {
         <Container>
 
           <h1>Main Title</h1>
-          <p>Qui tenetur amet nisi, voluptate totam voluptates possimus tempora assumenda vitae quas in earum voluptas ad maiores esse laboriosam ipsum. Deserunt eum nihil doloribus, nobis dolores quis nostrum quibusdam dolorum.
+          <p className='description'>Qui tenetur amet nisi, voluptate totam voluptates possimus tempora assumenda vitae quas in earum voluptas ad maiores esse laboriosam ipsum. Deserunt eum nihil doloribus, nobis dolores quis nostrum quibusdam dolorum.
           </p>
           <div className="btn-container">
             <a href="#" className="btn-link">Learn more</a>
